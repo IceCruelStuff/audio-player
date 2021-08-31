b0cffc8491c820eee43ef825b498f6282a3b577a
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-08-30 21:23:19 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const generateUUID = () => ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c => (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16));
+
 function handleForm(event) {
     event.preventDefault();
 }
@@ -10,4 +12,16 @@ document.getElementById("input").onsubmit = function() {
     element.autoplay = true;
     element.loop = true;
     element.play();
+    fetch(inputUrl).then(res => res.blob()).then(blob => {
+        var reader = new FileReader();
+        var fileByteArray = [];
+        reader.readAsArrayBuffer(blob);
+        reader.onloadend = function(event) {
+            if (event.target.readyState == FileReader.DONE) {
+                var byteArray = new Uint8Array(event.target.result);
+                var decoder = new TextDecoder('utf8');
+                localStorage.setItem(generateUUID(), btoa(decoder.decode(byteArray)));
+            }
+        }
+    });
 }
